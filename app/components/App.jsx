13ec40de8cd4aab17733667cd4ef9665fc92e819
Kirk Okada <kirkokada@gmail.com>
2017-02-17 11:45:38 -1000

    Push data to App

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import Notes from './Notes';
 
+const notes = [
+  {
+        id: uuid.v4(),
+        task: 'Learn React'
+  },
+  {
+        id: uuid.v4(),
+        task: 'Do laundry'
+  }
+];
+
 export default () => (
   <div>
     <button onClick={() => console.log('add note')}>+</button>
-    <Notes />
+    <Notes notes={notes} />
   </div>
 );
