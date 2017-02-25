28b153c5d83cd1feb3d3ac6a4949ef42a1f7218c
Kirk Okada <kirkokada@gmail.com>
2017-02-24 14:14:53 -1000

    Style individual notes

--- app/components/Editable.jsx
+++ app/components/Editable.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 
-export default ({editing, value, onEdit, ...props}) => {
+import classnames from 'classnames';
+
+export default ({editing, value, onEdit, className, ...props}) => {
   if(editing) {
-    return <Edit value={value} onEdit={onEdit} {...props} />;
+    return <Edit value={value}
+                 onEdit={onEdit}
+                 className={className}
+                 {...props} />;
   }
 
-  return <span {...props}>{value}</span>;
+  return <span className={classnames('value', className)}
+    {...props}>{value}</span>;
 }
 
 class Edit extends React.Component {
   render() {
-    const {value, onEdit, ...props} = this.props;
+    const {value, onEdit, className, ...props} = this.props;
 
     return <input
       type="text"
+      className={classnames('edit', className)}
       autoFocus={true}
       defaultValue={value}
       onBlur = {this.finishEdit}
